b8efecdf35e25c87dd8ea0e418e290a1c738d60f
Thomas Kristensen <thomas.kristensen@uswitch.com>
2013-10-23 08:56:51 +0100

    Truncate Dinesman puzzle to 50 chars per line.

--- src/hands_on.clj
+++ src/hands_on.clj
@@ -300,13 +300,16 @@
 ;; EXAMPLE: Dinesman's multiple-dwelling problem
 "Dinesman's multiple-dwelling problem
 
-Baker, Cooper, Fletcher, Miller, and Smith live on different floors of
-an apartment house that contains only five floors. Baker does not live
-on the top floor. Cooper does not live on the bottom floor. Fletcher
-does not live on either the top or the bottom floor. Miller lives on a
-higher floor than does Cooper. Smith does not live on a floor adjacent
-to Fletcher's. Fletcher does not live on a floor adjacent to
-Cooper's. Where does everyone live?"
+Baker, Cooper, Fletcher, Miller, and Smith live on
+different floors of an apartment house that
+contains only five floors. Baker does not live
+on the top floor. Cooper does not live on the
+bottom floor. Fletcher does not live on either the
+top or the bottom floor. Miller lives on a higher
+floor than does Cooper. Smith does not live on a
+floor adjacent to Fletcher's. Fletcher does not
+live on a floor adjacent to Cooper's. Where does
+everyone live?"
 
 ;; We are using latero, not-adjacento and permuteo
 (defn dinesman
